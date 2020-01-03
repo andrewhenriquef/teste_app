09483fb943d08e5aad5c7a74e030d12675f7e132
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-01-02 21:43:32 -0300

    (+) Add factory callbacks example

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -29,8 +29,14 @@ FactoryBot.define do
     factory :customer_male, traits: [:male]
     factory :customer_female, traits: [:female]
 
+    # this is a callback
     after(:create) do |customer, evaluator|
       customer.name.upcase! if evaluator.upcased
     end
+
+    # another kinds of callbacks
+    # after(:build), runs after the object be created in memory
+    # before(:create), runs before the object be persisted in database
+    # after(:create), runs after the object be persisted in database
   end
 end

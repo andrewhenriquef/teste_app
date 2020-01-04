29c78ae1d9b8f0b4d2418fa3619284ede5511d00
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-01-04 17:21:39 -0300

    (+) Add VCR com uri's não determinísticas

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'HTTParty' do
-  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts' } do
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
     # webmock example
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(
@@ -12,7 +12,7 @@ describe 'HTTParty' do
     #     }
     #   )
 
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end

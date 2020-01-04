e65992336afbe6dce0d83562c4c3f2e24d06a5df
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-01-03 21:53:43 -0300

    (+) Add create_list example

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe Order, type: :model do
 
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it 'create_list example' do
+    orders = create_list(:order, 3, description: 'teste')
+
+    orders.map do |order|
+      expect(order.description).to eq('teste')
+    end
+
+    expect(orders.count).to eq 3
+  end
 end

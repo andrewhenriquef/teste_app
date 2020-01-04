5d16ea3df2f270ec994f4c3d9d78eee2936c4ef3
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-01-04 17:04:38 -0300

    (+) Add VCR with metadata example

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe 'HTTParty' do
-  it 'content-type' do
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts' } do
+    # webmock example
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(
     #     status: 200,
@@ -11,10 +12,8 @@ describe 'HTTParty' do
     #     }
     #   )
 
-    VCR.use_cassette("jsonplaceholder/posts") do
-      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-      content_type = response.headers['content-type']
-      expect(content_type).to match(/application\/json/)
-    end
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
   end
 end

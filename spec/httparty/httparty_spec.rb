3c0ea174c1e7033d772f72d102d89b70565be2c7
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-01-04 17:33:31 -0300

    (+) Add VCR record modes example

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe 'HTTParty' do
-  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+  # it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', record: :new_episodes } do
     # webmock example
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(
@@ -11,6 +12,12 @@ describe 'HTTParty' do
     #       'content-type': 'application/json; chartset: utf-8'
     #     }
     #   )
+
+    # VCR record mods
+    # once, default
+    # new_episodes, every time that i change the url will record again
+    # none, never will generate a request
+    # all, will record always
 
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
     content_type = response.headers['content-type']

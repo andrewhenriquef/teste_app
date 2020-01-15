e7c133eb5c273809569ced19abea9a95c2bc73d6
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-01-14 21:44:22 -0300

    (+) Add examples to fill forms and test ajax calls with capybara

--- spec/features/customers_spec.rb
+++ spec/features/customers_spec.rb
@@ -7,4 +7,30 @@ RSpec.feature "Customers", type: :feature, js: true do
     # page.save_screenshot('screenshot.png')
     expect(page.current_path).to eq(customers_path)
   end
+
+  it 'create a new customer' do
+    member = create(:member)
+
+    login_as(member, scope: :member)
+
+    visit(new_customer_path)
+
+    expect {
+      fill_in('customer_name', with: Faker::Name.name)
+      fill_in('customer_email', with: Faker::Internet.email)
+      fill_in('customer_address', with: Faker::Address.street_address)
+
+      click_button('Create Customer')
+    }.to change { Customer.all.count }.by(1)
+
+    expect(page).to have_content('Customer was successfully created.')
+  end
+
+  it 'Ajax' do
+    visit(customers_path)
+
+    click_link('Add Message')
+
+    expect(page).to have_content('Yes!')
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,7 @@ end
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Warden::Test::Helpers
 
   config.include FactoryBot::Syntax::Methods
 

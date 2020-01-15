481f29e6463eb27c1181de44a57b7dda2e979620
Andrew Oliveira <andrewhenriquef@gmail.com>
2020-01-14 21:49:43 -0300

    (+) Add capybara find examples

--- spec/features/customers_spec.rb
+++ spec/features/customers_spec.rb
@@ -33,4 +33,11 @@ RSpec.feature "Customers", type: :feature, js: true do
 
     expect(page).to have_content('Yes!')
   end
+
+  it 'Find' do
+    visit(customers_path)
+
+    click_link('Add Message')
+    expect(find('#my-div').find('h1')).to have_content('Yes!')
+  end
 end
